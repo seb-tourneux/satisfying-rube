68882363cb264a8a54a3647dfa19b2b1b2e0d98e
Sebastien Tourneux <sebastientourneux1@gmail.com>
2024-12-08 14:37:34 +0100

    ADD base scene

--- create_scene.py
+++ create_scene.py
@@ -1,6 +1,7 @@
 import bpy
 import math
 import colorsys
+import time
 
 print("=Launch")
 
@@ -51,6 +52,11 @@ def clear_scene():
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete(use_global=False)
 
+def load_base_scene():
+    bpy.ops.wm.open_mainfile(filepath="ressources/base.blend")
+    # wait for the scene to be correctly loaded
+    time.sleep(1)
+
 def create_platform(i, frame, location, rotation, note):
     bpy.ops.mesh.primitive_cube_add(size=1, location=location)
     platform = bpy.context.object
@@ -61,32 +67,44 @@ def create_platform(i, frame, location, rotation, note):
     platform.rigid_body.type = 'PASSIVE'
     platform.rigid_body.friction = 0.1
     platform.rigid_body.restitution = 1
+    
+    if frame >= 0:
 
-    bpy.context.scene.frame_set(frame-1)
-    data_path = "color"
-    platform.keyframe_insert(data_path=data_path, frame=frame-1)
+        bpy.context.scene.frame_set(frame-1)
+        data_path_color = "color"
+        activated_prop = "Prop_activated"
+        data_path_activated = f'["{activated_prop}"]'
+        platform[activated_prop] = 0.0
+        platform.keyframe_insert(data_path=data_path_color, frame=frame-1)
+        platform.keyframe_insert(data_path=data_path_activated, frame=frame-1)
 
-    if note in note_colors_US:
-        platform.color = note_colors_US[note]
-    if note in note_colors_EU:
-        platform.color = note_colors_EU[note]
+        bpy.context.scene.frame_set(frame)
+        if note in note_colors_US:
+            platform.color = note_colors_US[note]
+        if note in note_colors_EU:
+            platform.color = note_colors_EU[note]
 
-    platform.keyframe_insert(data_path=data_path, frame=frame)
-    bpy.context.scene.frame_set(frame)
-    platform.keyframe_insert(data_path=data_path, frame=frame)
-
+        platform[activated_prop] = 1.0
+        platform.keyframe_insert(data_path=data_path_activated, frame=frame)
+        platform.keyframe_insert(data_path=data_path_color, frame=frame)
+    
+    mat = bpy.data.materials.get("M_platform_plate")
+    platform.data.materials.append(mat)
+        
     return platform
 
 def create_ball(location, radius=0.5):
-    bpy.ops.mesh.primitive_uv_sphere_add(radius=radius, location=location)
-    ball = bpy.context.object
-    ball.name = "Ball"
-    bpy.ops.rigidbody.object_add()
+    ball = bpy.data.objects.get("Ball")
     ball.rigid_body.type = 'ACTIVE'
     ball.rigid_body.mass = 1
     ball.rigid_body.friction = 0.1
     ball.rigid_body.restitution = ball_bounciness
     return ball
+
+def create_wall(ball_position_at_the_end):
+    wall = bpy.data.objects.get("wall")
+    wall.scale = (1, 100, abs(ball_position_at_the_end[2]) + 50)
+
 
 def run_simulation(ball, stop_frame):
     bpy.ops.ptcache.free_bake_all()
@@ -104,25 +122,39 @@ def run_simulation(ball, stop_frame):
     return None
 
 def setup_cameras():
-    # large camera
-    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW')
-    bpy.context.object.location = (100, -9, 24)
-    bpy.context.object.rotation_euler = (math.radians(73), 0, math.radians(80))
-
+    #empty
+    bpy.ops.object.empty_add(type='PLAIN_AXES', align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+    empty_loc = bpy.context.object
+    empty_loc.name = "E_Ball_loc"
     bpy.ops.object.constraint_add(type='COPY_LOCATION')
-    bpy.context.object.constraints["Copy Location"].target = bpy.data.objects["Ball"]
-    bpy.context.object.constraints["Copy Location"].use_offset = True
+    empty_loc.constraints["Copy Location"].target = bpy.data.objects["Ball"]
+    empty_loc.constraints["Copy Location"].use_offset = True
     
+    bpy.ops.object.empty_add(type='PLAIN_AXES', align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+    empty_rot = bpy.context.object
+    empty_rot.name = "E_Ball_rot"
+    empty_rot.rotation_euler = (math.radians(73), 0, math.radians(80))
+    empty_rot.parent = empty_loc
+        
     # close-up camera
     bpy.ops.object.camera_add(enter_editmode=False, align='VIEW')
-    bpy.context.object.location = (9, -1.7, 2.18)
-    bpy.context.object.rotation_euler = (math.radians(73), 0, math.radians(80))
+    cam = bpy.context.object
+    cam.parent = empty_rot
+    cam.name = "C_close"
+    cam.location[2] = 24
+    
+    # large camera
+    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW')
+    cam = bpy.context.object
+    cam.parent = empty_rot
+    cam.name = "C_large"
+    cam.location[2] = 50
 
-    bpy.ops.object.constraint_add(type='COPY_LOCATION')
-    bpy.context.object.constraints["Copy Location"].target = bpy.data.objects["Ball"]
-    bpy.context.object.constraints["Copy Location"].use_offset = True
 
 def generate_platforms_from_notes():
+    ball_position_at_end = (0,0,0)
+    ball = bpy.data.objects.get("Ball")
+
     for (i, frame_note) in enumerate(notes_list):
         (time, note, octave) = frame_note
         frame = int(time * frame_rate)
@@ -138,19 +170,23 @@ def generate_platforms_from_notes():
                         location=ball_position_at_end, 
                         rotation=(math.radians(platform_angle), 0, 0), 
                         note=note)
+    return ball_position_at_end
 
-
-
-if __name__ == '__main__':
-    clear_scene()
-
+def main():
     ball = create_ball(location=ball_start_location)
     setup_cameras()
-    create_platform(-1, 0,
+    create_platform(-1, -1,
                     location=(0,0,-20), 
                     rotation=(math.radians(20.0), 0, 0), 
                     note="")
     
-    generate_platforms_from_notes()
+    ball_position_at_end = generate_platforms_from_notes()
+
+    create_wall(ball_position_at_end)
+
+
+if __name__ == '__main__':
+    load_base_scene()
+    bpy.app.timers.register(main, first_interval=0.01)
 
 
